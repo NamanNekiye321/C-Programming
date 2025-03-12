08c8fcccb64f736125f0d23bb41374ba11a5c73d
NamanNekiye321 <namannekiye321@gmail.com>
2025-03-12 14:07:30 +0530

    commit

--- Chapter-3(Conditional Instructions)/ternary.c
+++ Chapter-3(Conditional Instructions)/ternary.c
@@ -2,11 +2,25 @@
 
 int main() {
     
-    int age = 5;
+    // int age = 5;
 
-    age == 5 ? printf("Its true\n") : printf("Its false\n");
+    // age == 5 ? printf("Its true\n") : printf("Its false\n");
 
     //ternary or conditional operator
+
+
+
+    int a ;
+
+    printf("Please enter a :\n");
+    scanf("%d",&a);
+
+    a < 5 ? printf("Value of a is smaller than 5 \n") : printf("Value of a is greater than 5\n");
+
+
+
+
+
 
 
 
