bca269deb8bab1a4f3018d0648dd0cd67818642d
NamanNekiye321 <namannekiye321@gmail.com>
2025-03-12 16:22:15 +0530

    commit

--- Chapter-3(Conditional Instructions)/switch_case.c
+++ Chapter-3(Conditional Instructions)/switch_case.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    int choice;
 
-    printf("Enter a number between 1 and 5: ");
-    scanf("%d", &choice);
+    int rating;
+    
+    printf("Please Enter your rating:\n");
+    scanf("%d",&rating);
 
-    switch (choice) {
-        case 1:
-            printf("You chose 1.\n");
-            break;
-        case 2:
-            printf("You chose 2.\n");
-            break;
-        case 3:
-            printf("You chose 3.\n");
-            break;
-        case 4:
-            printf("You chose 4.\n");
-            break;
-        case 5:
-            printf("You chose 5.\n");
-            break;
-        default:
-            printf("Invalid choice.\n");
-            break;
+    switch (rating)
+    {
+    case 1:
+        printf("Thank for 1 Rating\n");
+        break;
+    case 2:
+        printf("Thank for 2 Rating\n");
+        break;
+    case 3:
+        printf("Thank for 3 Rating\n");
+        break;
+    case 4:
+        printf("Thank for 4 Rating\n");
+        break;
+    case 5:
+        printf("Thank for 5 Rating\n");
+        break;
+    
+    default:
+        printf("Invalid");
+        break;
     }
+    
+
+
+
+
+
 
     return 0;
 }
